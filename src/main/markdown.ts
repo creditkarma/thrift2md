7f0176265372170b4f2e1646cff18f301b268538
Nick Nance <nick.nance@creditkarma.com>
2018-06-24 11:07:20 -0700

    text and blockquote support

--- src/main/markdown.ts
+++ src/main/markdown.ts
@@ -1,18 +1,16 @@
-export enum MarkdownHeaders {
+export enum MarkdownTypes {
     HeaderLevel1 = 'h1',
     HeaderLevel2 = 'h2',
     HeaderLevel3 = 'h3',
     HeaderLevel4 = 'h4',
     HeaderLevel5 = 'h5',
     HeaderLevel6 = 'h6',
-}
-
-export enum MarkdownText {
     Paragraph = 'p',
+    BlockQuote = 'blockquote',
 }
 
 export interface IMarkdown {
-    type: MarkdownHeaders | MarkdownText
+    type: MarkdownTypes
 }
 
 export interface IHeaderLevel1 extends IMarkdown { h1: string }
@@ -23,28 +21,36 @@ export interface IHeaderLevel5 extends IMarkdown { h5: string }
 export interface IHeaderLevel6 extends IMarkdown { h6: string }
 
 export interface IParagraph extends IMarkdown { p: string | string[] }
+export interface IBlockQuote extends IMarkdown { blockquote: string | string[] }
 
 export type MarkdownNode = IHeaderLevel1 | IHeaderLevel2 | IHeaderLevel3 | IHeaderLevel4 |
-                        IHeaderLevel5 | IHeaderLevel6 | IParagraph
+                        IHeaderLevel5 | IHeaderLevel6 | IParagraph | IBlockQuote
 
-export const header = (text: string, type: MarkdownHeaders):
+export const header = (text: string, type: MarkdownTypes):
     IHeaderLevel1 | IHeaderLevel2 | IHeaderLevel3 | IHeaderLevel4 | IHeaderLevel5 | IHeaderLevel6 => {
     switch (type) {
-        case MarkdownHeaders.HeaderLevel1: return {h1: text, type}
-        case MarkdownHeaders.HeaderLevel2: return {h2: text, type}
-        case MarkdownHeaders.HeaderLevel3: return {h3: text, type}
-        case MarkdownHeaders.HeaderLevel4: return {h4: text, type}
-        case MarkdownHeaders.HeaderLevel5: return {h5: text, type}
-        case MarkdownHeaders.HeaderLevel6: return {h6: text, type}
+        case MarkdownTypes.HeaderLevel1: return {h1: text, type}
+        case MarkdownTypes.HeaderLevel2: return {h2: text, type}
+        case MarkdownTypes.HeaderLevel3: return {h3: text, type}
+        case MarkdownTypes.HeaderLevel4: return {h4: text, type}
+        case MarkdownTypes.HeaderLevel5: return {h5: text, type}
+        case MarkdownTypes.HeaderLevel6: return {h6: text, type}
+        default: throw new Error(`markdownNodeMatch: Could not match type ${type}`)
     }
 }
 
 export const paragraph = (text: string | string[]): IParagraph => ({
     p: text,
-    type: MarkdownText.Paragraph,
+    type: MarkdownTypes.Paragraph,
+})
+
+export const blockquote = (text: string | string[]): IBlockQuote => ({
+    blockquote: text,
+    type: MarkdownTypes.BlockQuote,
 })
 
 interface IMarkdownPattern<T> {
+    BlockQuote: (_: IBlockQuote) => T
     HeaderLevel1: (_: IHeaderLevel1) => T
     HeaderLevel2: (_: IHeaderLevel2) => T
     HeaderLevel3: (_: IHeaderLevel3) => T
@@ -56,25 +62,29 @@ interface IMarkdownPattern<T> {
 
 function markdownNodeMatch<T>(p: IMarkdownPattern<T>, r: MarkdownNode): T {
     switch (r.type) {
-        case MarkdownHeaders.HeaderLevel1: return p.HeaderLevel1(r as IHeaderLevel1)
-        case MarkdownHeaders.HeaderLevel2: return p.HeaderLevel2(r as IHeaderLevel2)
-        case MarkdownHeaders.HeaderLevel3: return p.HeaderLevel3(r as IHeaderLevel3)
-        case MarkdownHeaders.HeaderLevel4: return p.HeaderLevel4(r as IHeaderLevel4)
-        case MarkdownHeaders.HeaderLevel5: return p.HeaderLevel5(r as IHeaderLevel5)
-        case MarkdownHeaders.HeaderLevel6: return p.HeaderLevel6(r as IHeaderLevel6)
-        case MarkdownText.Paragraph: return p.Paragraph(r as IParagraph)
-        default: throw new Error(`markdownNodeMatch: Could not match type ${typeof r}`)
+        case MarkdownTypes.HeaderLevel1: return p.HeaderLevel1(r as IHeaderLevel1)
+        case MarkdownTypes.HeaderLevel2: return p.HeaderLevel2(r as IHeaderLevel2)
+        case MarkdownTypes.HeaderLevel3: return p.HeaderLevel3(r as IHeaderLevel3)
+        case MarkdownTypes.HeaderLevel4: return p.HeaderLevel4(r as IHeaderLevel4)
+        case MarkdownTypes.HeaderLevel5: return p.HeaderLevel5(r as IHeaderLevel5)
+        case MarkdownTypes.HeaderLevel6: return p.HeaderLevel6(r as IHeaderLevel6)
+        case MarkdownTypes.Paragraph: return p.Paragraph(r as IParagraph)
+        case MarkdownTypes.BlockQuote: return p.BlockQuote(r as IBlockQuote)
     }
 }
 
+const arrayToStr = (a: string | string[], transform: (_: string) => string): string =>
+    Array.isArray(a) ? a.map(transform).join('') : transform(a)
+
 export const transformField = (fld: MarkdownNode): string => {
     return markdownNodeMatch({
-        HeaderLevel1: (_) => `# ${_.h1}\n\n`,
-        HeaderLevel2: (f) => `## ${f.h2}\n\n`,
-        HeaderLevel3: (g) => `### ${g.h3}\n\n`,
-        HeaderLevel4: (h) => `#### ${h.h4}\n\n`,
-        HeaderLevel5: (i) => `##### ${i.h5}\n\n`,
-        HeaderLevel6: (j) => `###### ${j.h6}\n\n`,
-        Paragraph: (_) => `${_.p}\n\n`,
+        BlockQuote: (node) => arrayToStr(node.blockquote, (_) => `> ${_}\n\n`),
+        HeaderLevel1: (node) => `# ${node.h1}\n\n`,
+        HeaderLevel2: (node) => `## ${node.h2}\n\n`,
+        HeaderLevel3: (node) => `### ${node.h3}\n\n`,
+        HeaderLevel4: (node) => `#### ${node.h4}\n\n`,
+        HeaderLevel5: (node) => `##### ${node.h5}\n\n`,
+        HeaderLevel6: (node) => `###### ${node.h6}\n\n`,
+        Paragraph: (node) => arrayToStr(node.p, (_) => `${_}\n\n`),
     }, fld)
 }
--- src/test/unit/markdown.spec.ts
+++ src/test/unit/markdown.spec.ts
@@ -1,8 +1,9 @@
 import { expect } from 'code'
 import * as Lab from 'lab'
 import {
+    blockquote,
     header,
-    MarkdownHeaders,
+    MarkdownTypes,
     transformField,
 } from '../../main/markdown'
 
@@ -12,22 +13,32 @@ const describe = lab.describe
 const it = lab.it
 const before = lab.before
 
+const content = 'Hello world'
+
 describe('When generating markdown for header1', () => {
-    const head = header('Hello world', MarkdownHeaders.HeaderLevel1)
+    const head = header(content, MarkdownTypes.HeaderLevel1)
 
     it('should have a string with a leading #', () => {
         expect(transformField(head)).to.contain('# ')
     })
 
     it('should contain # and value', () => {
-        expect(transformField(head)).to.equal('# Hello world\n\n')
+        expect(transformField(head)).to.equal(`# ${content}\n\n`)
     })
 })
 
 describe('When generating markdown for header2', () => {
-    const head = header('Hello world', MarkdownHeaders.HeaderLevel2)
+    const head = header(content, MarkdownTypes.HeaderLevel2)
 
     it('should have a string with a leading ##', () => {
         expect(transformField(head)).to.contain('## ')
+    })
+})
+
+describe('When generating markdown for blockquote', () => {
+    const text = blockquote(content)
+
+    it('should have a string with a leading >', () => {
+        expect(transformField(text)).to.equal(`> ${content}\n\n`)
     })
 })
