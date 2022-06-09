0230066f8fcc867d4eab7fe7f71029f18a4fae46
Gerard Reese <gerardr@microsoft.com>
2022-06-09 00:50:16 -0700

    add parameter for custom output path

--- src/bin/thrift2md.ts
+++ src/bin/thrift2md.ts
@@ -11,7 +11,8 @@ import { ThriftDocument } from '@creditkarma/thrift-parser';
 
 const args = process.argv.slice(2)
 const fileName = args[0]
-const outputFile = `./${parse(fileName).name}.md`
+const outputDir = args[1] ? (args[1].toString().endsWith('/') ? args[1] : args[1] + "/") : './'
+const outputFile = outputDir + `${parse(fileName).name}.md`
 
 const logDoc = (doc: ThriftDocument) => {
     console.dir(doc, {depth: null})
